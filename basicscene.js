ba39cf47585cd2228d6c74cf330403291f4ff09d
brancamp <brdaca@gmail.com>
2018-02-07 16:46:06 -0700

    update

--- basicscene.js
+++ basicscene.js
@@ -2,20 +2,20 @@ var width = window.innerWidth;
 var height = window.innerHeight;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
+renderer.setPixelRatio(window.devicePixelRatio); //this is so IMPORTANT!!! For textures to look sharp. 
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
 var boxGeo = new THREE.BoxGeometry(1, 1, 1);
 var boxMat = new THREE.MeshBasicMaterial({
     color: 0x00cccc
 });
 var box = new THREE.Mesh(boxGeo, boxMat);
-camera.position.set(40, 40, 40);
-camera.lookAt(scene.position);
+camera.position.set(60, 0, 60);
 scene.add(box);
-renderer.setSize(width, height);
-document.body.appendChild(renderer.domElement);
-
 window.addEventListener('resize', resizeHandler);
-
 update();
 
 
